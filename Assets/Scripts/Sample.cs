6678115b423f80ea5ea47f74f624218aaa52353b
Sunagimo <>
2019-05-19 18:52:47 +0900

    feat: init commit

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    #region Inspector
+    /// <summary>
+    /// ドロップダウンプレースホルダー。
+    /// </summary>
+    [Header("ドロップダウンプレースホルダー")]
+    [SerializeField]
+    DropdownPlaceHolder dropdownPlaceHolder;
+    #endregion
+
+    void Start()
+    {
+        dropdownPlaceHolder.SetPlaceHolderLbl("Please select");
+    }
+}
---
+++ Assets/Scripts/Sample2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Sample2 : MonoBehaviour
+{
+    /// <summary>
+    /// ドロップダウン。
+    /// </summary>
+    [Header("ドロップダウン")]
+    [SerializeField]
+    TMP_Dropdown dropdown = default;
+
+    void Start()
+    {
+        dropdown.options.Insert(0, new TMP_Dropdown.OptionData("Please select"));
+        dropdown.value = 0;
+    }
+}
